dc2cb0893006503b0f4a3e555f357591f8bd23c5
drexin <dario.rexin@r3-tech.de>
2010-10-22 14:16:43 +0200

    changed version number to 0.9.0 because of the near complete state and extended the readme

--- lib/safe-me/version.rb
+++ lib/safe-me/version.rb
@@ -1,6 +1,6 @@
 module SafeMe
   unless defined?(SafeMe::VERSION)
-    VERSION       = '0.0.1'
+    VERSION       = '0.9.0'
     LIBDIR        = File.expand_path(File.dirname(__FILE__) + '/../../lib')
   end
 end
